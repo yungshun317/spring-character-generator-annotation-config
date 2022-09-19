e3a63480607fcf9ba8a0d1cdf044de6d7ad9b20e
yungshun317 <seraph317@gmail.com>
2022-09-19 23:37:38 +0800

    [CHORE] Create a Spring project using Spring Initializr

---
+++ src/main/java/yungshun/chang/springcharactergeneratorannotationconfig/SpringCharacterGeneratorAnnotationConfigApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springcharactergeneratorannotationconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCharacterGeneratorAnnotationConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCharacterGeneratorAnnotationConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yungshun/chang/springcharactergeneratorannotationconfig/SpringCharacterGeneratorAnnotationConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springcharactergeneratorannotationconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCharacterGeneratorAnnotationConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
