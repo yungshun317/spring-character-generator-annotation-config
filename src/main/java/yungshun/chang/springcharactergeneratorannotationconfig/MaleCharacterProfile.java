50a824c9764de2db2dd871a0a21f417251c34392
yungshun317 <seraph317@gmail.com>
2022-09-20 08:04:55 +0800

    [FEAT] Create setter methods in class and configure the setter injection with `@Autowired`

--- src/main/java/yungshun/chang/springcharactergeneratorannotationconfig/MaleCharacterProfile.java
+++ src/main/java/yungshun/chang/springcharactergeneratorannotationconfig/MaleCharacterProfile.java
@@ -8,8 +8,22 @@ public class MaleCharacterProfile implements CharacterProfile {
 
     private PersonalityService personalityService;
 
+    /* Constructor injection
     @Autowired
     public MaleCharacterProfile(PersonalityService personalityService) {
+        this.personalityService = personalityService;
+    }
+     */
+
+    // Define a default constructor
+    public MaleCharacterProfile() {
+        System.out.println(">> MaleCharacterProfile: inside default constructor");
+    }
+
+    // Define a setter method
+    @Autowired
+    public void setPersonalityService(PersonalityService personalityService) {
+        System.out.println(">> MaleCharacterProfile: inside setPersonalityService() method");
         this.personalityService = personalityService;
     }
 
